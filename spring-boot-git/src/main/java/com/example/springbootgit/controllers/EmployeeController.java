8a0cee0a70dab3660a85a93d957fb7a6a26f8daa
Aleksandra <isaisic1@gmail.com>
2019-08-26 20:08:27 +0200

    Employee controller

---
+++ spring-boot-git/src/main/java/com/example/springbootgit/controllers/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.example.springbootgit.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+	
+	@GetMapping
+	public String index() {
+		return "Hello from employee";
+	}
+}
